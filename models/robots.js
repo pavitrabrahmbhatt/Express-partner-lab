1e30dd579861293af8b101d6cab2fdee72e4a922
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-17 14:36:25 -0500

    Created data array

---
+++ models/robots.js
@@ -0,0 +1,17 @@
+const robots = [
+	{
+		name: 'bob',
+		size: 10,
+		color: 'red'
+	},
+	{
+		name: 'rick',
+		size: 20,
+		color: 'blue',
+	},
+	{
+		name: 'stan',
+		size: 15,
+		color: 'green'
+	}
+]
